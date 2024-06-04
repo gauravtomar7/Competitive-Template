8e498dab057ec5a94bcb6f7258963b8b2ac558f6
Gaurav Singh Tomar <116975109+gauravtomar7@users.noreply.github.com>
2024-06-04 17:02:45 +0530

    Update Segment_Tree.cpp

--- Segment_Tree.cpp
+++ Segment_Tree.cpp
@@ -1,49 +1,49 @@
 class SGTree {
-      vector<int> seg;
+      vector<ll> seg;
 public:
-      SGTree(int n) {
+      SGTree(ll n) {
             seg.resize(4 * n + 1);
       }
-
+ 
       //O(N)
-      void build(int ind, int low, int high, int arr[]) {
+      void build(ll ind, ll low, ll high, ll arr[]) {
             if (low == high) {
                   seg[ind] = arr[low];
                   return;
             }
-
-            int mid = (low + high) / 2;
+            ll mid = (low + high) / 2;
             build(2 * ind + 1, low, mid, arr);
             build(2 * ind + 2, mid + 1, high, arr);
-            seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);        //change
+            seg[ind] = (seg[2 * ind + 1] + seg[2 * ind + 2]);        //change
       }
-
+ 
       //O(LOGN)
-      int query(int ind, int low, int high, int l, int r) {
+      ll query(ll ind, ll low, ll high, ll l, ll r) {
             // no overlap
             // l r low high or low high l r
-            if (r < low || high < l) return INT_MAX;          //change
+            if (r < low || high < l) return 0;          //change
  
             // complete overlap
             // [l low high r]
             if (low >= l && high <= r) return seg[ind];
-
-            int mid = (low + high) >> 1;
-            int left = query(2 * ind + 1, low, mid, l, r);
-            int right = query(2 * ind + 2, mid + 1, high, l, r);
-            return min(left, right);                                    //change
+ 
+            ll mid = (low + high) >> 1;
+            ll left = query(2 * ind + 1, low, mid, l, r);
+            ll right = query(2 * ind + 2, mid + 1, high, l, r);
+            return (left + right);                                    //change
       }
-
+ 
       //O(LOGN)
-      void update(int ind, int low, int high, int i, int val) {
+      void update(ll ind, ll low, ll high, ll i, ll val) {
             if (low == high) {
                   seg[ind] = val;
                   return;
             }
-
-            int mid = (low + high) >> 1;
+ 
+            ll mid = (low + high) >> 1;
             if (i <= mid) update(2 * ind + 1, low, mid, i, val);
             else update(2 * ind + 2, mid + 1, high, i, val);
-            seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);                   //change
+            seg[ind] = (seg[2 * ind + 1] + seg[2 * ind + 2]);                   //change
       }  
 };
+   
